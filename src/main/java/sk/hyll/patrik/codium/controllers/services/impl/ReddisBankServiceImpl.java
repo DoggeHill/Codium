24b6e94fe9450bfcabd74a1d0dbbaceeff051b6d
DoggeHill <patrikhyll@gmail.com>
2021-11-30 02:19:03 +0100

    Update redis settings

--- src/main/java/sk/hyll/patrik/codium/controllers/services/impl/ReddisBankServiceImpl.java
+++ src/main/java/sk/hyll/patrik/codium/controllers/services/impl/ReddisBankServiceImpl.java
@@ -7,11 +7,19 @@ import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 import sk.hyll.patrik.codium.controllers.services.BankService;
 import sk.hyll.patrik.codium.mappers.BankMapper;
+import sk.hyll.patrik.codium.model.BankCard;
 import sk.hyll.patrik.codium.model.CardOwner;
+import sk.hyll.patrik.codium.model.CreditCard;
+import sk.hyll.patrik.codium.model.State;
 import sk.hyll.patrik.codium.model.dto.BankDTO;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 @Profile("local")
@@ -25,11 +33,27 @@ public class ReddisBankServiceImpl implements BankService {
         this.bankCardMapper = bankCardMapper;
         this.jedis = new Jedis();
         this.objectMapper = new ObjectMapper();
+        this.objectMapper.setDateFormat(new SimpleDateFormat("dd MM yyyy"));
     }
 
     @Override
-    public void createAndAddCard() {
-        // TODO: dummy data
+    public void createDummyData() {
+        CardOwner cardOwner = new CardOwner();
+        cardOwner.setName("Dummy");
+        cardOwner.setSurname("Dummy");
+        String dateString = "12 10 2010";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy", Locale.ENGLISH);
+        LocalDate localeDate = LocalDate.parse(dateString, formatter);
+
+        BankCard creditCard= new CreditCard(44057790, java.sql.Date.valueOf(localeDate), "001", "visa", State.ACTIVE, 100.5);
+        cardOwner.addCard(creditCard);
+        String data = null;
+        try {
+            data = this.objectMapper.writeValueAsString(cardOwner);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        this.jedis.set(cardOwner.getSurname(),data);
     }
 
     @Override
--- src/main/java/sk/hyll/patrik/codium/init/RedisInit.java
+++ src/main/java/sk/hyll/patrik/codium/init/RedisInit.java
@@ -1,16 +1,24 @@
 package sk.hyll.patrik.codium.init;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import sk.hyll.patrik.codium.controllers.services.BankService;
 import sk.hyll.patrik.codium.model.CardOwner;
 
 @Configuration
 @Profile("local")
-public class RedisInit {
+public class RedisInit implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Value("${spring.dummy.generate}")
+    private String active;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         return new JedisConnectionFactory();
@@ -21,5 +29,16 @@ public class RedisInit {
         RedisTemplate<CardOwner, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
         return template;
+    }
+    BankService bankCardService;
+    public RedisInit(BankService bankCardService) {
+        this.bankCardService = bankCardService;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+
+        if(this.active.equals("true"))
+        bankCardService.createDummyData();
     }
 }
